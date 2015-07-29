0afcf94ddecbeed00e9aee2a67d6096c9b029d6d
@sgtn <shogo1104@gmail.com>
2015-07-29 19:00:06 +0900

    text js

--- app/View/index.php
+++ app/View/index.php
@@ -1,6 +1,5 @@
-<?php include __PUBLIC__ . '/template/header.php' ?>
-
-<script src="index_view.js"></script>
-<script src="index_controller.js"></script>
-<script src="index"></script>
-<?php include __PUBLIC__ . '/template/footer.php'; ?>
+<?php include __PUBLIC__ . 'template/header.php' ?>
+<script src="<?php echo __PUBLIC__; ?>app/View/index_view.js"></script>
+<script src="<?php echo __PUBLIC__; ?>app/Controller/index_controller.js"></script>
+<script src="<?php echo __PUBLIC__; ?>app/Model/index.js"></script>
+<?php include __PUBLIC__ . 'template/footer.php'; ?>
---
+++ public/app/Model/index.js
@@ -0,0 +1,9 @@
+(function(global){
+  function _A(){}
+  _A.prototype.a = function(){
+
+  }
+
+  global._A = _A;
+  return global;
+}(window));
