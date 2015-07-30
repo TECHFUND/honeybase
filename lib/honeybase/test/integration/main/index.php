92e8aa823b625b4265a5909fd29ce3fad7f541a6
@sgtn <shogo1104@gmail.com>
2015-07-30 14:01:06 +0900

    first spec successed

--- lib/honeybase/test/integration/main/index.php
+++
@@ -1,7 +0,0 @@
-<?php include __PUBLIC__ . 'template/header.php' ?>
-  <link rel="shortcut icon" type="image/png" href="../webdriver/jasmine-2.3.4/jasmine_favicon.png">
-  <link rel="stylesheet" type="text/css" href="../webdriver/jasmine-2.3.4/jasmine.css">
-  <script type="text/javascript" src="../webdriver/jasmine-2.3.4/jasmine.js"></script>
-  <script type="text/javascript" src="../webdriver/jasmine-2.3.4/jasmine-html.js"></script>
-  <script type="text/javascript" src="../webdriver/jasmine-2.3.4/boot.js"></script>
-<?php include __PUBLIC__ . 'template/footer.php'; ?>
--- lib/honeybase/test/routes.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$app->get('/', function () { return view("test.integration.main.index"); });
