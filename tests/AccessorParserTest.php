225d7dff5094b976609b038cc641caf72692e9bb
shogochiai <shogo1104@gmail.com>
2015-08-06 20:02:54 +0900

    test worked before function needed

--- tests/AccessorParserTest.php
+++ tests/AccessorParserTest.php
@@ -1,18 +1,17 @@
 <?php
 
 use Util\Util\Util;
+use Util\Util\NuLog;
 use HoneyBase\Core\Middleware\AccessorParser;
 
 class AccessorParserTest extends TestCase {
 
   // DI
   public function testSetAccessor(){
-    $accessor = Util::getJSON("accessor.json");
+    $accessor = Util::getJSON(__DIR__."/accessor.json");
     $parser = new AccessorParser();
     $result = $parser->setAccessor($accessor);
     $this->assertTrue($result);
-
-    $this->parser = $this->parser;
   } // str->bool
 
 
@@ -21,40 +20,32 @@ class AccessorParserTest extends TestCase {
   */
   // Existance of "table input" effect to "register" or "database"
   public function testIsDatabase(){
-    $result = $this->parser->isDatabase("issues");
+    $accessor = Util::getJSON(__DIR__."/accessor.json");
+    $parser = new AccessorParser();
+    $result = $parser->isDatabase("issues");
     $this->assertTrue($result);
-
-    $this->parser = $this->parser;
   } // str->bool
 
   // Set keys to matcher
   // database
   public function testClimbTableBranch(){
-    $result = $this->parser->climbTableBranch("issues");
+    $result = $parser->climbTableBranch("issues");
     $this->assertTrue($result);
-
-    $this->parser = $this->parser;
   } // str->bool
   public function testClimbActionBranch(){
-    $result = $this->parser->climbActionBranch("insert");
+    $result = $parser->climbActionBranch("insert");
     $this->assertTrue($result);
-
-    $this->parser = $this->parser;
   } // str->bool
   public function testClimbRoleBranch(){
-    $result = $this->parser->climbRoleBranch("admin");
+    $result = $parser->climbRoleBranch("admin");
     $this->assertTrue($result);
-
-    $this->parser = $this->parser;
   } // str->bool // current_userのrole
 
   // Set matcher
   // In the database context, params input in the HTTP Request are matcher
   public function testMatchParamsToAccessor(){
-    $result = $this->parser->matchParamsToAccessor(["body", "created_at", "updated_at"]);
+    $result = $parser->matchParamsToAccessor(["body", "created_at", "updated_at"]);
     $this->assertTrue($result);
-
-    $this->parser = $this->parser;
   } // array -> bool
 
 
@@ -64,24 +55,18 @@ class AccessorParserTest extends TestCase {
   */
   // Set keys to matcher
   public function testClimbPathBranch(){
-    $result = $this->parser->climbPathBranch("/");
+    $result = $parser->climbPathBranch("/");
     $this->assertTrue($result);
-
-    $this->parser = $this->parser;
   } // str->bool
 
   // Set matcher
   // In the register context, provider&role inputs in the HTTP Request are matchers
   public function testMatchRoleToAccessor(){
-    $result = $this->parser->matchRoleToAccessor("admin");
+    $result = $parser->matchRoleToAccessor("admin");
     $this->assertTrue($result);
-
-    $this->parser = $this->parser;
   } // str->bool
   public function testMatchProviderToAccessor(){
-    $result = $this->parser->matchProviderToAccessor("facebook");
+    $result = $parser->matchProviderToAccessor("facebook");
     $this->assertTrue($result);
-
-    $this->parser = $this->parser;
   } // str->bool
 }
